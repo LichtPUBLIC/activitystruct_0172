e0a6734a2324605be3e29625eb1a9fbee36367d8
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-04-02 12:10:38 +0700

    membuat input dan output alamat mhs yg berisi desa dan kota

--- Struktur2/Struktur2.cpp
+++ Struktur2/Struktur2.cpp
@@ -20,4 +20,10 @@ int main() {
 	cout << "Nama Mahasiswa :";
 	cin >> mhs.nama;
 
+	cout << "alamat mahasiswa : "<<endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
+
 	
