499717a222beaa05314fa1bbf86ea508e85538f3
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-04-02 12:43:24 +0700

    membuat input dan output nama mhs

--- Struktur4/Struktur4.cpp
+++ Struktur4/Struktur4.cpp
@@ -18,3 +18,6 @@ int main() {
 		cout << "data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor mahasiswa : ";
 		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
+
