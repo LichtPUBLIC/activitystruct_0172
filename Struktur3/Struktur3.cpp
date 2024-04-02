e16961d27470d99cc5f050c535a7de510b3eb343
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-04-02 12:29:23 +0700

    membuat input  dan output alamat yg berisi desa dan kota

--- Struktur3/Struktur3.cpp
+++ Struktur3/Struktur3.cpp
@@ -19,4 +19,9 @@ int main() {
 	cout << "Nama Mahasiswa : ";
 	getline(cin, mhs.nama);
 
+	cout << "alamat mahasiswa : " << endl;
+	cout << "\t nama Desa : ";
+	cin >> mhs.alamat.desa;
+	cout << "\t Nama kota : ";
+	cin >> mhs.alamat.kota;
 	
